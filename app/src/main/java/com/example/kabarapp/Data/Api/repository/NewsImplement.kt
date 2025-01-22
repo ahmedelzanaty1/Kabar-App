fae1893036410cf430eabb9bc64d024228974d22
Hp <ahmedelzanaty001@gmail.com>
2025-01-22 14:06:59 +0200

    Home Screen Get News (Api)

---
+++ app/src/main/java/com/example/kabarapp/Data/Api/repository/NewsImplement.kt
@@ -0,0 +1,27 @@
+package com.example.kabarapp.Data.Api.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.kabarapp.Data.Api.ApiPagingSources
+import com.example.kabarapp.Data.Api.Article
+import com.example.kabarapp.Data.Api.NewsApi
+import kotlinx.coroutines.flow.Flow
+
+class NewsImplement(
+    private val newsApi: NewsApi
+) : NewsRepository {
+    override fun getnews(sources: List<String>): Flow<PagingData<Article>> {
+        return Pager(
+            config = PagingConfig(pageSize = 10),
+            pagingSourceFactory = {
+                ApiPagingSources(
+                    api = newsApi,
+                    sources = sources.joinToString(separator = ",")
+                )
+            }
+        ).flow
+    }
+
+}
+
