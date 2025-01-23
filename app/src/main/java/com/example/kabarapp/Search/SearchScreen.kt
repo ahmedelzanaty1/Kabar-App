42e3ad21ba65bc18803ef5edc1659fab9aa806f3
Hp <ahmedelzanaty001@gmail.com>
2025-01-23 14:18:29 +0200

    Search Screen

--- app/src/main/java/com/example/kabarapp/Search/SearchScreen.kt
+++ app/src/main/java/com/example/kabarapp/Search/SearchScreen.kt
@@ -21,7 +21,7 @@ fun SearchScreen(
 
     Column(
         modifier = Modifier
-            .padding(top = 4.dp, start = 4.dp, end = 4.dp)
+            .padding(top = 5.dp, start = 5.dp, end = 5.dp)
             .statusBarsPadding()
     ) {
         SearchBar(
